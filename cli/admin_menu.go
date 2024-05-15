70236fb5112c4500e7c9faf4b80dfecfaa1d0965
olanseverson <olanseverson13@gmail.com>
2024-05-15 10:55:13 +0700

    fix: remove .env

--- cli/admin_menu.go
+++ cli/admin_menu.go
@@ -1,7 +1,16 @@
 package main
 
-import "vapor/entity"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"vapor/entity"
+)
 
 func adminMenu(u entity.User) {
-	
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Selamat datang ADMIN, ", u.Username)
+	fmt.Println()
+	fmt.Println("Silahkan pilih menu:")
+
 }
