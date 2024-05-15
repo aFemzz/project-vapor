e3a3be8ff8553b2e22e32dfc3266d33b8eb18701
febbryandika <febbryandika97@gmail.com>
2024-05-15 15:17:56 +0700

    feat: add library features

--- handler/library.go
+++ handler/library.go
@@ -1,5 +1,49 @@
 package handler
 
-func Library() {
+import (
+	"fmt"
+	"vapor/config"
+	"vapor/entity"
+	"vapor/utility"
+)
 
+func Library(user entity.User) {
+	db, err := config.GetDB()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("============")
+	fmt.Println("  Library")
+	fmt.Println("============")
+
+	query := "SELECT g.title FROM games g JOIN order_details od ON g.game_id = od.game_id JOIN orders o ON od.order_id = o.order_id WHERE o.user_id = ?"
+
+	rows, err := db.Query(query, user.User_ID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer rows.Close()
+
+	index := 1
+	data := false
+	for rows.Next() {
+		data = true
+		var title string
+
+		if err = rows.Scan(&title); err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("%d. %s\n", index, title)
+	}
+
+	if !data {
+		fmt.Println("No game in your library")
+	}
+
+	fmt.Println()
+	utility.EnterToContinue()
 }
