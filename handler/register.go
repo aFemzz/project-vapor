c690347857feb3339976d2d452c8025b08aec86c
olanseverson <olanseverson13@gmail.com>
2024-05-16 17:59:01 +0700

    feat: add unit test for register handler

--- handler/register.go
+++ handler/register.go
@@ -1,75 +1,36 @@
 package handler
 
 import (
-	"bufio"
-	"database/sql"
 	"fmt"
-	"os"
-	"strings"
-	"vapor/config"
-	"vapor/entity"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
-func Register() {
-	var u entity.User
-	db, err := config.GetDB()
-	if err != nil {
-		fmt.Println("Error when connecting to db:", err)
-		return
-	}
-	defer db.Close()
-
-	fmt.Println("======================================")
-	fmt.Println("         REGISTER NEW ACCOUNT ")
-	fmt.Println("======================================")
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Insert your username: ")
-	username, _ := reader.ReadString('\n')
-	username = strings.TrimSpace(username)
-
-	fmt.Print("Insert your email: ")
-	email, _ := reader.ReadString('\n')
-	email = strings.TrimSpace(email)
-
-	fmt.Print("Insert your password: ")
-	password, _ := reader.ReadString('\n')
-	password = strings.TrimSpace(password)
+func (h *Handler) Register(username, password, email string) error {
 
 	hashedPass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		fmt.Println("Error while encrypting password")
-		return
+		return fmt.Errorf("error while encrypting password")
 	}
 
-	_, err = db.Exec("INSERT INTO users (username, password, email, role, saldo) VALUES(?,?,?,?,?)", username, hashedPass, email, "user", 0.0)
+	// Check if the unique column value already exists
+	var exists bool
+	err = h.DB.QueryRow("SELECT EXISTS(SELECT 1 FROM users WHERE email = ?)", email).Scan(&exists)
 	if err != nil {
-		fmt.Println("Error when registering user:", err)
-		return
+		return err
+	}
+
+	// If the unique column value exists, handle it according to your requirements
+	if exists {
+		return fmt.Errorf("data with email '%s' already exists", email)
+	}
+
+	query := "INSERT INTO users (username, password, email, role, saldo) VALUES(?,?,?,?,?)"
+	_, err = h.DB.Exec(query, username, hashedPass, email, "user", 0.0)
+	if err != nil {
+		return fmt.Errorf("error when registering user:%v", err)
 	}
 
 	fmt.Printf("User '%v' added succesfuly!\n", username)
-	fmt.Println("Your funds is zero, do you want to add funds? (y/n)")
-	for {
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSpace(input)
-		switch input {
-		case "y":
-			// get the user data
-			err = db.QueryRow("SELECT user_id, username, password, role, saldo FROM users WHERE email = ?", email).Scan(&u.User_ID, &u.Username, &u.Password, &u.Role, &u.Saldo)
-			switch {
-			case err == sql.ErrNoRows:
-				fmt.Println("password or user doesn't match")
-			case err != nil:
-				fmt.Printf("error: %v\n", err.Error())
-			}
-			AddFunds(u)
-			return
-		case "n":
-			return
-		default:
-			fmt.Println("invalid input")
-		}
-	}
+	return nil
 }
---
+++ handler/register_test.go
@@ -0,0 +1,75 @@
+package handler
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRegister_UserExist(t *testing.T) {
+	// Create a new mock DB
+	mockDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("error creating mock database: %v", err)
+	}
+	defer mockDB.Close()
+
+	// Create a new handler with the mock DB
+	handler := &Handler{
+		DB: mockDB,
+	}
+
+	// Mock the SELECT query to return false (user does not exist)
+	mock.ExpectQuery("SELECT EXISTS").
+		WithArgs("yol@gmail.com").
+		WillReturnRows(sqlmock.NewRows([]string{"exists"}).AddRow(true))
+
+		// Call the AddAdmin function
+	err = handler.Register("yol", "yol", "yol@gmail.com")
+
+	// Assert that correct error returned
+	assert.Error(t, err)
+	assert.EqualError(t, err, fmt.Sprintf("data with email '%s' already exists", "yol@gmail.com"))
+
+	// Verify that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestRegister_NewUser(t *testing.T) {
+	// Create a new mock DB
+	mockDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("error creating mock database: %v", err)
+	}
+	defer mockDB.Close()
+
+	// Create a new handler with the mock DB
+	handler := &Handler{
+		DB: mockDB,
+	}
+
+	// Mock the SELECT query to return false (user does not exist)
+	mock.ExpectQuery("SELECT EXISTS").
+		WithArgs("user@example.com").
+		WillReturnRows(sqlmock.NewRows([]string{"exists"}).AddRow(false))
+
+	// Mock the INSERT query to succeed
+	mock.ExpectExec("INSERT INTO users").
+		WithArgs("new_user", sqlmock.AnyArg(), "user@example.com", "user", 0.0).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	// Call the Register function
+	err = handler.Register("new_user", "yol", "user@example.com")
+
+	// Assert that correct error returned
+	assert.NoError(t, err)
+
+	// Verify that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
