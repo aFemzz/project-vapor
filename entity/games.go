5226341ba68a0e04615b900c5d9e0264d721bf0f
olanseverson <olanseverson13@gmail.com>
2024-05-16 14:18:43 +0700

    feat: add unit test for addGame

---
+++ entity/games.go
@@ -0,0 +1,11 @@
+package entity
+
+type Games struct {
+	GameID      int
+	Title       string
+	Description string
+	Price       float64
+	Developer   string
+	Publisher   string
+	Rating      float64
+}
--- handler/addGame.go
+++ handler/addGame.go
@@ -1,65 +1,29 @@
 package handler
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
-	"vapor/config"
+	"vapor/entity"
 )
 
-func AddGame() {
-	db, err := config.GetDB()
+func (s *Handler) AddGame(g entity.Games) error {
+
+	// Check if the unique column value already exists
+	var exists bool
+	err := s.DB.QueryRow("SELECT EXISTS(SELECT 1 FROM games WHERE title = ?)", g.Title).Scan(&exists)
 	if err != nil {
-		fmt.Println("Error when connecting to db:", err)
-		return
-	}
-	defer db.Close()
-
-	fmt.Println("======================================")
-	fmt.Println("             ADD NEW GAME ")
-	fmt.Println("======================================")
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Insert Game Title: ")
-	title, _ := reader.ReadString('\n')
-	title = strings.TrimSpace(title)
-
-	fmt.Print("Insert Description: ")
-	description, _ := reader.ReadString('\n')
-	description = strings.TrimSpace(description)
-
-	fmt.Print("Insert Game Price : ")
-	price, _ := reader.ReadString('\n')
-	price = strings.TrimSpace(price)
-	priceF, err := strconv.ParseFloat(price, 64)
-	if err != nil {
-		fmt.Println("Invalid input!!")
-		return
+		return err
 	}
 
-	fmt.Print("Insert Game Developer : ")
-	developer, _ := reader.ReadString('\n')
-	developer = strings.TrimSpace(developer)
-
-	fmt.Print("Insert Game Publisher : ")
-	publisher, _ := reader.ReadString('\n')
-	publisher = strings.TrimSpace(publisher)
-
-	fmt.Print("Insert Game Rating : ")
-	rating, _ := reader.ReadString('\n')
-	rating = strings.TrimSpace(rating)
-	ratingF, err := strconv.ParseFloat(rating, 64)
-	if err != nil {
-		fmt.Println("Invalid input!!")
-		return
+	// If the unique column value exists, handle it
+	if exists {
+		return fmt.Errorf("game with title '%s' already exists", g.Title)
 	}
-
-	_, err = db.Exec("INSERT INTO games (title, description, price, developer, publisher, rating, is_deleted) VALUES(?,?,?,?,?,?,?)", title, description, priceF, developer, publisher, ratingF, false)
+	query := "INSERT INTO games (title, description, price, developer, publisher, rating, is_deleted) VALUES(?,?,?,?,?,?,?)"
+	_, err = s.DB.Exec(query, g.Title, g.Description, g.Price, g.Developer, g.Publisher, g.Rating, false)
 	if err != nil {
-		fmt.Println("Error when registering new game:", err)
-		return
+		return fmt.Errorf("error when registering new game: %v", err)
 	}
 
 	fmt.Println("New game added succesfuly!")
+	return nil
 }
